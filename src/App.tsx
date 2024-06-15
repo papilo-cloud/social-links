b662f5a0f1e80d14469a9b4627b6070acbc75098
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2024-06-15 01:24:20 +0100

    demonstrated how to use the profile-link-card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import SocialLinkProfile from "./components/core";
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <div className="flex justify-center items-center h-screen bg-black-off">
+      <SocialLinkProfile.Root>
+          <SocialLinkProfile.Avatar url="" />
+          <SocialLinkProfile.ProfileInfo>
+              <SocialLinkProfile.Typography.Name>Abdul Badamasi</SocialLinkProfile.Typography.Name>
+              <SocialLinkProfile.Typography.Location>Kaduna, Nigeria</SocialLinkProfile.Typography.Location>
+          </SocialLinkProfile.ProfileInfo>
+          <SocialLinkProfile.Typography.BodyText>"Front-end developer and avid raeder"</SocialLinkProfile.Typography.BodyText>
+          <SocialLinkProfile.LinksRoot>
+              <SocialLinkProfile.Links>Github</SocialLinkProfile.Links>
+              <SocialLinkProfile.Links>Frontend Mentor</SocialLinkProfile.Links>
+          </SocialLinkProfile.LinksRoot>
+      </SocialLinkProfile.Root>
+    </div>
   )
 }
 
